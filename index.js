31ecfe22197bae1ac4d90dc894709904f9422a8d
yperdoom <pedro.pinto@unoesc.edu.br>
2021-11-12 09:39:07 -0300

    comunicacao serial

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("uaauauau")
+const express = require('express')
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('COM5')
+
+
+const parser = port.pipe(new Readline({delimiter: '\n'}))
+parser.on('data', console.log)
+
+//console.log(parser)
+const connectionSerial = parser
+
+
+console.log(connectionReceived)
